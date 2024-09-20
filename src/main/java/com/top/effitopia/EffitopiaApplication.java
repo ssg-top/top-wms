724bec317f4fabafe2e6d3bbafe677f88a51fc5e
kinggora <jhha09@ajou.ac.kr>
2024-09-20 17:28:07 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/top/effitopia/EffitopiaApplication.java
@@ -0,0 +1,13 @@
+package com.top.effitopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffitopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffitopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/top/effitopia/EffitopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.top.effitopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffitopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
