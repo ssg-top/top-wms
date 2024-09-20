4b8e37f6344ca452bab9d82acd2fbda931710ee7
kinggora <jhha09@ajou.ac.kr>
2024-09-20 19:12:50 +0900

    :bento:: 리소스 파일 추가
    부트스트랩 정적 파일(js, css, scss), 프로필 이미지 파일

--- src/main/java/com/top/effitopia/EffitopiaApplication.java
+++ src/main/java/com/top/effitopia/EffitopiaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EffitopiaApplication {
+public class  EffitopiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EffitopiaApplication.class, args);
