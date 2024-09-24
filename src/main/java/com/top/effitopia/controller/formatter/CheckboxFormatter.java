0a7069fcdcdd9a668cac648197633393cccc13c3
leejuhwan <seungjoon0324@gmail.com>
2024-09-24 09:53:46 +0900

    📦: CheckboxFormatter, LocalDateTimeFormatter 생성

---
+++ src/main/java/com/top/effitopia/controller/formatter/CheckboxFormatter.java
@@ -0,0 +1,21 @@
+package com.top.effitopia.controller.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+public class CheckboxFormatter implements Formatter<Boolean> {
+    @Override
+    public Boolean parse(String text, Locale locale) throws ParseException {
+        if (text==null){
+            return false;
+        };
+        return text.equals("true");
+    }
+
+    @Override
+    public String print(Boolean object, Locale locale) {
+        return object.toString();
+    }
+}
