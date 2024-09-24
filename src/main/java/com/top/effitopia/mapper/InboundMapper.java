6168659f6d35207e00a474c15b80456945843fe3
Park Taehwan <xoghks211@naver.com>
2024-09-24 18:06:51 +0900

    🚧 입고 Service, Mapper 일부 기능 정의
    기능 구현을 위해 필요한 메서드들을 정의하고 dto, domain 변환을 위해 메서드를 추가하였습니다.

--- src/main/java/com/top/effitopia/mapper/InboundMapper.java
+++ src/main/java/com/top/effitopia/mapper/InboundMapper.java
@@ -1,5 +1,29 @@
 package com.top.effitopia.mapper;
 
-public class InboundMapper {
+import com.top.effitopia.domain.Inbound;
+import com.top.effitopia.dto.InboundSearchCond;
+import java.util.List;
+import java.util.Optional;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface InboundMapper {
+
+    int insert(Inbound inbound);
+
+    int update(Inbound inbound);
+
+    int delete(int inboundId);
+
+    Optional<Inbound> selectOne(int inboundId);
+
+    //List<Inbound> selectAllList(Pageable pageable);
+
+    int insertList(List<Inbound> inboundList);
+
+    int updateList(List<Inbound> inboundList);
+
+    int deleteList(List<Integer> inboundIds);
+
+    boolean existsByXxx(InboundSearchCond inboundSearchCond);
 }
