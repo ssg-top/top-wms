2cd81a50f59b4a3547bfdc55ce0da7dd2e85f177
kinggora <jhha09@ajou.ac.kr>
2024-09-25 17:48:47 +0900

    ✨회원 정보 수정 기능 추가

--- src/main/java/com/top/effitopia/service/AuthService.java
+++ src/main/java/com/top/effitopia/service/AuthService.java
@@ -1,59 +1,28 @@
 package com.top.effitopia.service;
 
-import com.top.effitopia.domain.Address;
-import com.top.effitopia.domain.Member;
-import com.top.effitopia.enumeration.MemberRole;
-import com.top.effitopia.enumeration.MemberStatus;
 import com.top.effitopia.dto.JoinDTO;
-import com.top.effitopia.exception.BizException;
-import com.top.effitopia.exception.ErrorCode;
-import com.top.effitopia.mapper.MemberMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.top.effitopia.dto.MemberDTO;
+import com.top.effitopia.enumeration.MemberStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
-public class AuthService {
+public interface AuthService {
 
-    private final MemberMapper memberMapper;
-    private final BCryptPasswordEncoder passwordEncoder;
 
-    public boolean join(JoinDTO joinDTO) {
-        if(checkDuplicateUsername(joinDTO.getUsername())) {
-            throw new BizException(ErrorCode.DUPLICATE_USERNAME);
-        } else if(checkDuplicateEmail(joinDTO.getEmail())) {
-            throw new BizException(ErrorCode.DUPLICATE_EMAIL);
-        } else if(MemberRole.BUSINESS_PROPRIETOR.equals(joinDTO.getRole()) && joinDTO.getBusinessNumber() == null) {
-            // TODO: 사업자 번호 유효성 검증
-            throw new BizException(ErrorCode.INVALID_BUSINESS_NUMBER);
-        }
-        Member member = Member.builder()
-                .username(joinDTO.getUsername())
-                .password(passwordEncoder.encode(joinDTO.getPassword()))
-                .name(joinDTO.getName())
-                .phone(joinDTO.getPhone())
-                .email(joinDTO.getEmail())
-                .role(joinDTO.getRole())
-                .status(MemberStatus.REGISTER_REQUEST)
-                .address(Address.builder()
-                        .zipCode(joinDTO.getZipCode())
-                        .roadNameAddress(joinDTO.getRoadNameAddress())
-                        .lotNumberAddress(joinDTO.getLotNumberAddress())
-                        .detailAddress(joinDTO.getDetailAddress())
-                        .build())
-                .businessNumber(joinDTO.getBusinessNumber())
-                .build();
-        return memberMapper.insert(member) > 0;
-    }
+    MemberDTO getOne(int id);
 
-    public boolean checkDuplicateUsername(String username) {
-        return memberMapper.existsByUsername(username);
-    }
+    List<MemberDTO> getList();
 
-    public boolean checkDuplicateEmail(String email) {
-        return memberMapper.existsByEmail(email);
-    }
+    boolean modify(MemberDTO memberDTO);
+    boolean modifyPassword();
+    boolean modifyStatus(int id, MemberStatus memberStatus);
 
+    boolean save(JoinDTO joinDTO);
+
+    boolean checkDuplicateUsername(String username);
+
+    boolean checkDuplicateEmail(String email);
 
 }
