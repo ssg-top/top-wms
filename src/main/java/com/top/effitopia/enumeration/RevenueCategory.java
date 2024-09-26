afd179f15c92d4c8801d40867bd185060a61cfda
kinggora <jhha09@ajou.ac.kr>
2024-09-26 18:01:05 +0900

    ✨지출, 매출 카테고리 추가

--- src/main/java/com/top/effitopia/enumeration/RevenueCategory.java
+++ src/main/java/com/top/effitopia/enumeration/RevenueCategory.java
@@ -1,4 +1,18 @@
 package com.top.effitopia.enumeration;
 
+import lombok.Getter;
+
+@Getter
 public enum RevenueCategory {
+    CONTRACT_COST("창고 계약비"),
+    STORAGE_CHARGE("창고 보관료"),
+    INBOUND_CHARGE("입고 수수료"),
+    OUTBOUND_CHARGE("출고 수수료"),
+    FREIGHT_CHARGE("운임료");
+
+    private final String value;
+
+    RevenueCategory(String value) {
+        this.value = value;
+    }
 }
