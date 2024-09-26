3df60ce6cbf2a8e34205c0d7440d7be4cc3ef7a4
kinggora <jhha09@ajou.ac.kr>
2024-09-26 18:05:17 +0900

    🚧인증 컨트롤러 구현 중

--- src/main/java/com/top/effitopia/controller/AuthController.java
+++ src/main/java/com/top/effitopia/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.top.effitopia.controller;
 
 import com.top.effitopia.dto.JoinDTO;
-import com.top.effitopia.service.AuthService;
+import com.top.effitopia.security.CustomUserDetails;
+import com.top.effitopia.service.AuthServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -14,11 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthServiceImpl authServiceImpl;
 
     @PostMapping("/join")
     public String join(JoinDTO joinDTO) {
-        authService.join(joinDTO);
+        authServiceImpl.save(joinDTO);
         return "join ok";
+    }
+
+    @GetMapping("/login")
+    public void loginPage() {
+        log.info("login page ok");
+    }
+
+    @GetMapping("/user")
+    public void admin(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        if(userDetails == null) {
+            log.info("user details is null");
+        } else {
+            log.info("user: {}", userDetails.getUsername());
+            userDetails.getAuthorities().forEach(authority -> log.info("role: {}", authority.getAuthority()));
+        }
     }
 }
