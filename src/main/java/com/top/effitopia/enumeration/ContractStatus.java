2e6a56e84621fde2a21a2a1e1e171c10abb5374e
nam3280 <nam3280@gmail.com>
2024-09-27 05:55:55 +0900

    📝 계약 상태 업데이트
    계약 상태 enum 업데이트

--- src/main/java/com/top/effitopia/enumeration/ContractStatus.java
+++ src/main/java/com/top/effitopia/enumeration/ContractStatus.java
@@ -1,7 +1,16 @@
 package com.top.effitopia.enumeration;
 
+import lombok.Getter;
+
+@Getter
 public enum ContractStatus {
-    APPROVE,
-    REJECT,
-    REQUEST
+    APPROVE("승인"),
+    REJECT("요청 중"),
+    REQUEST("거절");
+
+    private final String value;
+
+    ContractStatus(String value) {
+        this.value = value;
+    }
 }
