7d7818db69ee76c23c2ffe8349b22be63ad092a3
leejuhwan <seungjoon0324@gmail.com>
2024-09-27 01:42:01 +0900

    ♻️CheckoutMapper

--- src/main/java/com/top/effitopia/mapper/CheckoutMapper.java
+++ src/main/java/com/top/effitopia/mapper/CheckoutMapper.java
@@ -2,23 +2,23 @@ package com.top.effitopia.mapper;
 
 import com.top.effitopia.domain.Checkout;
 import com.top.effitopia.domain.CheckoutAnswer;
-import com.top.effitopia.domain.CheckoutQuestion;
-import com.top.effitopia.dto.CheckoutDTO;
 import com.top.effitopia.dto.PageRequestDTO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface CheckoutMapper {
-    List<Checkout> selectList(PageRequestDTO pageRequestDTO);
-
-    int getTotalCount(PageRequestDTO pageRequestDTO);
 
     int insertCheckout(Checkout checkout);
 
     int insertCheckoutAnswer(CheckoutAnswer checkoutAnswer);
 
-    Checkout getCheckoutDetails(Integer checkoutId);
+    List<Checkout> selectList(PageRequestDTO pageRequestDTO);
+
+    Checkout selectCheckoutById(Integer checkoutId);
+
+    int getTotalCount(PageRequestDTO pageRequestDTO);
+
+    List<Integer> getAllQuestionIds();
 }
