b8525420087be41dfb524f32768e1c3ae381413d
Park Taehwan <xoghks211@naver.com>
2024-09-27 18:32:39 +0900

    ✨ 거래처 CRUD 기능 구현 거래처 CRUD 기능 구현 완료하였습니다.

--- src/main/java/com/top/effitopia/service/VendorServiceImpl.java
+++ src/main/java/com/top/effitopia/service/VendorServiceImpl.java
@@ -1,5 +1,59 @@
 package com.top.effitopia.service;
 
-public class VendorServiceImpl {
+import com.top.effitopia.domain.Inbound;
+import com.top.effitopia.domain.Vendor;
+import com.top.effitopia.dto.VendorDTO;
+import com.top.effitopia.mapper.VendorMapper;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class VendorServiceImpl implements VendorService{
+
+    private final VendorMapper vendorMapper;
+    private final ModelMapper modelMapper;
+
+
+    @Override
+    public boolean save(VendorDTO vendorDTO) {
+        Vendor vendor = modelMapper.map(vendorDTO, Vendor.class);
+        return vendorMapper.insert(vendor) > 0;
+    }
+
+    @Override
+    public boolean modify(VendorDTO vendorDTO) {
+        Vendor vendor = modelMapper.map(vendorDTO, Vendor.class);
+        return vendorMapper.update(vendor) > 0;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return vendorMapper.delete(id) > 0;
+    }
+
+    @Override
+    public Optional<VendorDTO> get(int vendorId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<VendorDTO> getList() {
+        return null;
+    }
+
+    @Override
+    public void saveList(List<VendorDTO> vendorDTOList) {
+
+    }
+
+    @Override
+    public void removeList(List<Integer> vendorIdList) {
+
+    }
 }
