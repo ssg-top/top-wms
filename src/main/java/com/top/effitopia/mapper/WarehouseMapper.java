456505ca61edd32fadbb2fcddb734d8a2cead8e1
nam3280 <nam3280@gmail.com>
2024-09-29 00:48:58 +0900

    📝 쿼리문 업데이트
    쿼리문 join 추가

--- src/main/java/com/top/effitopia/mapper/WarehouseMapper.java
+++ src/main/java/com/top/effitopia/mapper/WarehouseMapper.java
@@ -2,6 +2,7 @@ package com.top.effitopia.mapper;
 
 import com.top.effitopia.domain.Cell;
 import com.top.effitopia.domain.Warehouse;
+import com.top.effitopia.dto.CellDTO;
 import com.top.effitopia.dto.PageRequestDTO;
 import com.top.effitopia.dto.WarehouseDTO;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,8 +12,8 @@ import java.util.Optional;
 
 @Mapper
 public interface WarehouseMapper {
-    List<Warehouse> selectWarehouseList(PageRequestDTO pageRequestDTO);
-    List<Cell> selectCellList(PageRequestDTO pageRequestDTO);
+    List<Warehouse> selectWarehouseList(PageRequestDTO<Warehouse> pageRequestDTO);
+    List<Cell> selectCellList(PageRequestDTO<Cell> pageRequestDTO);
     int update(Warehouse warehouse);
     int delete(Integer warehouse_id);
     Warehouse selectId(Integer id);
