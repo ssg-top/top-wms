98dcc01cc0ba36bbf54ea2bbe37bd0bd5061ade3
Park Taehwan <xoghks211@naver.com>
2024-09-30 10:02:30 +0900

    🎨 상품 코드 구조 개선
    원할한 기능구현을 위해 상품 코드 구조를 개선하였습니다.

--- src/main/java/com/top/effitopia/dto/ProductDTO.java
+++ src/main/java/com/top/effitopia/dto/ProductDTO.java
@@ -1,6 +1,8 @@
 package com.top.effitopia.dto;
 
 
+import com.top.effitopia.domain.Product;
+import com.top.effitopia.domain.ProductSubclassCategory;
 import com.top.effitopia.enumeration.ProductStorageType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,18 +16,60 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductDTO {
 
-    private Integer id;              //상품아이디
-    private String subclassCategory;    //소분류
-    private String name;         //상품명
-    private String productBrand;        //상품브랜드
-    private ProductStorageType productStorageType;  //상품보관타입
-    private String productCost;         //상품원가
-    private String productSelling;      //상품판매가
-    private String productInfo;         //상품정보
-    private String inboundBoxWidth;     //입고박스너비
-    private String inboundBoxLength;    //입고박스길이
-    private String InboundBoxHeight;    //입고박스높이
-    private Long productWeight;         //상품무게
-    private String productImg;          //상품이미지(주소)
+    private Integer id;
+    private ProductSubclassCategory productSubclassCategory;
+    private String name;
+    private String productBrand;
+    private ProductStorageType productStorageType;
+    private Integer productCost;
+    private Integer productSelling;
+    private String productInfo;
+    private Integer inboundBoxWidth;
+    private Integer inboundBoxLength;
+    private Integer inboundBoxHeight;
+    private Long productWeight;
+    private String productImg;
+
+    public Product toEntity() {
+        return Product.builder()
+            .id(this.id)
+            .productSubclassCategory(ProductSubclassCategory.builder()
+                .id(this.productSubclassCategory.getId())
+                .name(this.productSubclassCategory.getName())
+                .build())
+            .name(this.name)
+            .productBrand(this.productBrand)
+            .productStorageType(this.productStorageType)
+            .productCost(this.productCost)
+            .productSelling(this.productSelling)
+            .productInfo(this.productInfo)
+            .inboundBoxWidth(this.inboundBoxWidth)
+            .inboundBoxLength(this.inboundBoxLength)
+            .inboundBoxHeight(this.inboundBoxHeight)
+            .productWeight(this.productWeight)
+            .productImg(this.productImg)
+            .build();
+    }
+
+    public static ProductDTO fromEntity(Product product) {
+        return ProductDTO.builder()
+            .id(product.getId())
+            .productSubclassCategory(ProductSubclassCategory.builder()
+                .id(product.getProductSubclassCategory().getId())
+                .name(product.getProductSubclassCategory().getName())
+                .build())
+            .name(product.getName())
+            .productBrand(product.getProductBrand())
+            .productStorageType(product.getProductStorageType())
+            .productCost(product.getProductCost())
+            .productSelling(product.getProductSelling())
+            .productInfo(product.getProductInfo())
+            .inboundBoxWidth(product.getInboundBoxWidth())
+            .inboundBoxLength(product.getInboundBoxLength())
+            .inboundBoxHeight(product.getInboundBoxHeight())
+            .productWeight(product.getProductWeight())
+            .productImg(product.getProductImg())
+            .build();
+    }
 
 }
