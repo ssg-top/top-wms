127b84a00d0b3a272f54876f93f6f6bf0a841e61
Park Taehwan <xoghks211@naver.com>
2024-09-30 10:00:16 +0900

    🚧 거래처 기능구현 후 컨트롤러 작업중 거래처 컨트롤러 작업 중입니다.

--- src/main/java/com/top/effitopia/controller/VendorController.java
+++ src/main/java/com/top/effitopia/controller/VendorController.java
@@ -1,5 +1,14 @@
 package com.top.effitopia.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequestMapping("/vendors")
+@RequiredArgsConstructor
 public class VendorController {
 
 }
--- src/main/java/com/top/effitopia/dto/VendorDTO.java
+++ src/main/java/com/top/effitopia/dto/VendorDTO.java
@@ -1,6 +1,7 @@
 package com.top.effitopia.dto;
 
 import com.top.effitopia.domain.Inbound;
+import com.top.effitopia.domain.Vendor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,28 @@ public class VendorDTO {
 
     private Integer id;
     private String name;
-    private boolean delFlag;
+    private String phone;
+    private String address;
+    private boolean deleted;
+
+    public Vendor toEntity() {
+        return Vendor.builder()
+            .id(this.id)
+            .name(this.name)
+            .phone(this.phone)
+            .address(this.address)
+            .deleted(this.deleted)
+            .build();
+    }
+
+    public static VendorDTO fromEntity(Vendor vendor) {
+        return VendorDTO.builder()
+            .id(vendor.getId())
+            .name(vendor.getName())
+            .phone(vendor.getPhone())
+            .address(vendor.getAddress())
+            .deleted(vendor.isDeleted())
+            .build();
+    }
 
 }
