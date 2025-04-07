77d4dbdb5eb8ea4a809deee691400f79fb538d65
nam3280 <nam3280@naver.com>
2025-04-08 00:08:09 +0900

    ♻️ recycle: 코드 리팩토링

--- src/main/java/com/top/effitopia/config/ModelMapperConfig.java
+++ src/main/java/com/top/effitopia/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.top.effitopia.config;
 
+import com.top.effitopia.domain.Stock;
+import com.top.effitopia.dto.StockDTO;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,13 @@ public class ModelMapperConfig {
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.addMappings(new PropertyMap<Stock, StockDTO>() {
+            @Override
+            protected void configure() {
+                map(source.getCell(), destination.getCellDTO());
+                map(source.getProduct(), destination.getProductDTO());
+            }
+        });
         return modelMapper;
     }
 }
